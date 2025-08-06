131f337539908f81485728e4a9ffeda2afd9e12a
marcelofassi <marcelo.fassi@gmail.com>
2025-08-06 11:52:30 -0300

    Agregado de URL correcta para los servicios, se deja from funcionando

--- Front/src/api/axios.ts
+++ Front/src/api/axios.ts
@@ -1,8 +1,11 @@
 // src/api/axios.ts
 import axios from "axios";
 
+// Verificá la URL base que se está usando desde .env
+console.log("🌐 API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // desde el .env
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
